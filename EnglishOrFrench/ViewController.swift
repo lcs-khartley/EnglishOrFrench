3693a6307dc64cc96af0323d17e891e618bb53b1
Keira Hartley <khartley@lcs.on.ca>
2019-11-26 09:49:49 -0500

    Initial Commit

---
+++ EnglishOrFrench/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EnglishOrFrench
+//
+//  Created by Hartley, Keira on 2019-11-26.
+//  Copyright © 2019 Hartley, Keira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
